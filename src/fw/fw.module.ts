f5a5c8551cea85ab759327328920b507e187e267
Vitaliy Krushinsky <vitaliykrushinsky@gmail.com>
2017-04-10 12:39:30 +0300

    add few services and components

--- src/fw/fw.module.ts
+++ src/fw/fw.module.ts
@@ -1,8 +1,12 @@
+import { ScreenService } from './services/screen.service';
+import { StatusBarComponent } from './status-bar/status-bar.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 import { TitleBarComponent } from './title-bar/title-bar.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FrameworkBodyComponent } from './framework-body/framework-body.component';
 import { ContentComponent } from './content/content.component';
+import { FrameworkConfigService } from './services/framework-config.service';
 
 @NgModule({
   imports: [
@@ -11,7 +15,13 @@ import { ContentComponent } from './content/content.component';
   declarations: [
     FrameworkBodyComponent,
     ContentComponent,
-    TitleBarComponent
+    TitleBarComponent,
+    TopBarComponent,
+    StatusBarComponent
+  ],
+  providers: [
+    FrameworkConfigService,
+    ScreenService
   ],
   exports: [
     FrameworkBodyComponent
